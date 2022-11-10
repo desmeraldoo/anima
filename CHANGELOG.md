41c838348e54c8a73673309d109365baa23a77d0
dez <desmeraldoo@gmail.com>
2022-11-10 01:35:44 -0500

    Refurbish combo and provide it useful tabular output

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.1.0
+
+- Refurbish combo command to help with complicated multiattack scenarios
+
 ## 0.0.0
 
 - Restructure repo into package form
--- src/anima/core.py
+++ src/anima/core.py
@@ -1,9 +1,53 @@
 import cmd
+import itertools
 import math
 import random
 import traceback
+from bdb import BdbQuit
+from dataclasses import dataclass, field
+from typing import Final
+
+from tabulate import tabulate
 
 from anima.const import POWERS
+
+DUPLICATE_MARKER: Final[str] = "."
+
+
+@dataclass
+class AttackResolver:
+    """Stores information related to a single attack-defense exchange in Anima:
+    Beyond Fantasy.
+
+    Order matters in this dataclass for how the columns are organized in
+    the output table for combo. Upgrading data storage to pandas will
+    likely fix this.
+    """
+
+    _id: int
+    attack: int
+    defense: int
+    armor: int
+
+    base_damage: int | None = None
+
+    result: str = field(init=False)
+    damage: float = field(init=False)
+
+    def __post_init__(self) -> None:
+        self.result = calc_attack(
+            self.attack, self.defense, self.armor, self.base_damage
+        )
+        if "ATTACK" or "DAMAGE" in self.result:
+            damage_percentage = calc_damage_multiple(
+                self.attack - self.defense, self.armor
+            )
+            if self.base_damage is not None:
+                self.damage = damage_percentage * self.base_damage
+            else:
+                self.damage = damage_percentage
+        else:
+            self.damage = 0
 
 
 def parse(args: str) -> tuple[int, ...]:
@@ -50,23 +94,82 @@ def calc_attack(
         return f"DAMAGE: {int(round(base_damage * damage_multiple, 0))}"
 
 
-def calc_combo() -> str:
-    attacks = list(map(int, input("Enter final attack rolls: ").split(" ")))
-    defenses = list(map(int, input("Enter final defense rolls: ").split(" ")))
-    if len(attacks) != len(defenses):
-        return "ERROR: Number of attacks and defenses do not match. Enter 0 for missing defenses."
-    armor = int(input("Enter armor value (AT): "))
-    base_damage = int(input("Enter Base Damage: "))
+def format_extended(lst: list) -> list[int]:
+    result = []
+    dupe = 0
+    for idx in range(len(lst)):
+        item = lst[idx]
+        if item is None or item == DUPLICATE_MARKER:
+            result.append(dupe)
+        else:
+            dupe = int(item)
+            result.append(int(item))
+    return result
 
-    results = []
-    total_damage = 0
-    for attack, defense in zip(attacks, defenses):
-        if attack > defense:
-            damage_multiple = calc_damage_multiple(attack - defense, armor)
-            if damage_multiple > 0:
-                total_damage += int(round(base_damage * damage_multiple, 0))
-        results.append(calc_attack(attack, defense, armor, base_damage))
-    return "\n".join(results) + f"\nTOTAL DAMAGE: {total_damage}"
+
+def calc_combo() -> str:
+    """Calculates a combo attack in Anima, which is a combination of various
+    attacks, defenses, armor and damage values. By its nature, requires
+    additional input from the user.
+
+    Example of a combo of a Limited Additional Attack +4, Damage Augmentation (Single) +100 combo against a DR creature:
+    ```
+        atks:   150 160 170 180 190
+        defs:
+        amrs:   10
+        dmgs:   50 . . . 150
+
+    ```
+    """
+    atks_input = [_ for _ in input("atks:\t").split(" ") if _ != ""]
+    defs_input = [_ for _ in input("defs:\t").split(" ") if _ != ""]
+    amrs_input = [_ for _ in input("amrs:\t").split(" ") if _ != ""]
+    dmgs_input = [_ for _ in input("dmgs:\t").split(" ") if _ != ""]
+
+    # Extends each list to be the length of the longest list by filling in later positions with `None`
+    atks_extended, defs_extended, amrs_extended, dmgs_extended = map(
+        list,
+        zip(
+            *[
+                _
+                for _ in itertools.zip_longest(
+                    atks_input, defs_input, amrs_input, dmgs_input
+                )
+            ]
+        ),
+    )
+
+    atks_formatted = format_extended(atks_extended)
+    defs_formatted = format_extended(defs_extended)
+    amrs_formatted = format_extended(amrs_extended)
+    dmgs_formatted = format_extended(dmgs_extended)
+
+    resolutions = []
+    total = 0.0
+    for idx, (attack, defense, armor, damage) in enumerate(
+        zip(atks_formatted, defs_formatted, amrs_formatted, dmgs_formatted)
+    ):
+        resolution = AttackResolver(
+            _id=idx, attack=attack, defense=defense, armor=armor, base_damage=damage
+        )
+        resolutions.append(resolution)
+        total += resolution.damage
+    resolution_headers = [
+        "id",
+        "attack",
+        "defense",
+        "armor",
+        "base_damage",
+        "result",
+        "damage",
+    ]
+
+    resolution_table = tabulate(
+        resolutions, headers=resolution_headers, tablefmt="fancy_grid"  # type: ignore[arg-type] # justification: tabulate supports lists of dataclasses, but mypy doesn't understand this
+    )
+    total_table = tabulate([[total]], headers=["Total"], tablefmt="fancy_grid")
+
+    return f"{resolution_table}\n{total_table}"
 
 
 def calc_crit(level: int, phr_roll: int, location_id: int = 0) -> str:
@@ -214,11 +317,15 @@ class AnimaShell(cmd.Cmd):
         raise KeyboardInterrupt
 
 
-if __name__ == "__main__":
+def main() -> None:
     while True:
         try:
             AnimaShell().cmdloop()
-        except KeyboardInterrupt:
+        except (KeyboardInterrupt, BdbQuit):
             raise SystemExit(0)
         except:
             traceback.print_exc()
+
+
+if __name__ == "__main__":
+    main()
